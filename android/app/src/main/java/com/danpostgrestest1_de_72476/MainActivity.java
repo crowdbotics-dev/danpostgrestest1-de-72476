b9857ae1c70101a8ef1ba11611941db9751c56f5
Crowdbotics <engineering@crowdbotics.com>
2023-03-27 14:39:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/danpostgrestest1_de_72476/MainActivity.java
@@ -0,0 +1,15 @@
+package com.danpostgrestest1_de_72476;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "danpostgrestest1_de_72476";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "danpostgrestest1-de-72476",
+  url: "https://danpostgrestest1-de-72476.botics.co",
+  api: "https://danpostgrestest1-de-72476.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
